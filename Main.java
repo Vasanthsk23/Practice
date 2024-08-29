4fb62e4b3ceb954fd3cffad2689f88932330ef3c
Vasanthsk23 <vasanthk2312@gmail.com>
2024-08-29 12:11:57 +0530

    Added branch

--- Main.java
+++ Main.java
@@ -1,11 +1,15 @@
 
 import java.util.Scanner;
 public class Main {
+
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
         System.out.println(Math.min(a,b));
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.multiplyExact(a,b));
     }
 }
 
